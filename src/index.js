af888bc030664f797c9a881bda7159d92772b156
LenaYork <lenayork@tut.by>
2019-03-22 12:54:42 +0300

    feat: correct lexical mistake

--- src/index.js
+++ src/index.js
@@ -6,17 +6,13 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
   if  (preferences.length < 2) return 0;
   let triangles = 0;
-  
-   preferences.forEach((preference, index, preferences) => {
-    const spenochee1 = preferences[preference - 1];
-    const spenochee2 = preferences[spenochee1 - 1];
-  
- /* for (let i = 0; i < preferences.length; i++) {
+ 
+for (let i = 0; i < preferences.length; i++) {
         let spenochee1 = i+1;
         let spenochee2 = preferences[i];
         let spenochee3 = preferences[spenochee2-1];  
         if ((spenochee1 !== spenochee2) && (spenochee2 !== spenochee3) && (spenochee1 !== spenochee3)) {
-             if (spenochee1===preferences[sp3-1]) {
+             if (spenochee1===preferences[spenochee3-1]) {
                triangles++;
              } 
         }
@@ -24,11 +20,7 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
      
      
   return triangles > 0 ? triangles/3 : 0;
-};*/
-     
-     
-     if (spenochee2 === index + 1 && spenochee1 !== spenochee2) triangles++;
-  });
-  return (triangles / 3).toFixed(0);
 };
+     
+
                        
