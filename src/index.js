c431c225cbd0fbc25563f1a0c8fe0fb1ba4e340d
LenaYork <lenayork@tut.by>
2019-03-22 12:13:34 +0300

    feat: add trialgles code

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let triangles = 0;
+  for (let i = 0; i < preferences.length; i++) {
+        let spenochee1 = i+1;
+        let spenochee2 = preferences[i];
+        let spenochee3 = preferences[spenochee2-1];  
+        if ((spenochee1 !== spenochee2) && (spenochee2 !== spenochee3) && (spenochee1 !== spenochee3)) {
+             if (spenochee1===preferences[sp3-1]) {
+               triangles++;
+             } 
+        }
+  }//return triangle > 0 ? triangle/3 : 0;
+  
+   if (triangles > 0) {
+    let resut = triangles / 3;
+   } else result = 0;
+  return result;
 };
